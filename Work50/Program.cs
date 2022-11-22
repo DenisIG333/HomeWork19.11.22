ab09df796fe1e87dd2938e201cfdbbe07551f843
DenisIG333 <ignatovdnc@inbox.ru>
2022-11-22 20:27:29 +0300

    Домашнее задание

---
+++ Work50/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+/* Задача 50. Напишите программу, которая на вход принимает позиции
+элемента в двумерном массиве, и возвращает значение этого элемента или же указание,
+что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет */
+    
+
+void RandomArray(int[,] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+
+void Print(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int ReadUser(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int rows = ReadUser("Введите строку: ");
+int colums = ReadUser("Введите столбец: ");
+int[,] numbers = new int[7, 9];
+RandomArray(numbers);
+Print(numbers);
+
+
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1))
+{
+    Console.WriteLine($" Значение элемента -> {numbers[rows, colums]}");
+}
+else Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
---
+++ Work52/Program.cs
@@ -0,0 +1,62 @@
+﻿/* 
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее
+арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. 
+*/
+
+Console.WriteLine("количество строк");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("количество столбцов");
+int m = int.Parse(Console.ReadLine());
+
+int[,] numbers = new int[n, m];
+RandomArray(numbers);
+Console.WriteLine();
+
+Print(numbers);
+Console.WriteLine();
+
+Console.Write("Среднее арифметическое каждого столбца: ");
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = avarage + numbers[i, j];
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+
+
+void RandomArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void Print(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+      
+        Console.WriteLine("");
+    }
+}
+
+
